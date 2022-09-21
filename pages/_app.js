6d508725c237ad1e975f983bd3dd7eb24ca695ad
josevaldo <josevaldosicuba@gmail.com>
2022-09-21 14:54:26 +0100

    Modal

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,9 @@ import { NextUIProvider } from '@nextui-org/react';
 import Head from 'next/head';
 import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
+  
   return (
+    
     <>
       <Head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
